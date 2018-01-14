37540704180e723ef9a32bb9f93f6a481e5a5060
majid <majid@gmail.com>
2018-01-14 22:08:35 +0330

    income add

--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return self.text
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    amount = models.BigIntegerField()
+
+    def __str__(self):
+        return '{}-{}'.format(self.date,self.text)
+
+
+
